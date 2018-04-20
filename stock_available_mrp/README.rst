b7b6f23f427a8ca5b4d27944cfef027e6168feb2
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-04-20 18:09:57 +0200

    [MIG] stock_available_mrp: Migration to 11.0

--- stock_available_mrp/README.rst
+++ stock_available_mrp/README.rst
@@ -1,5 +1,5 @@
-.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
    :alt: License: AGPL-3
 
 =========================================================
@@ -15,7 +15,6 @@ with the components available to promise.
 
 Usage
 =====
-
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
    :target: https://runbot.odoo-community.org/runbot/153/8.0
@@ -69,6 +68,8 @@ Contributors
 * Lionel Sausin (Numérigraphe) <ls@numerigraphe.com>
 * many thanks to Graeme Gellatly for his advice and code review
 * Laurent Mignon <laurent.mignon@acsone.eu>
+
+Do not contact contributors directly about support or help with technical issues.
 
 Maintainer
 ----------
--- stock_available_mrp/models/product_product.py
+++ stock_available_mrp/models/product_product.py
@@ -1,25 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2014 Numérigraphe SARL
+# Copyright 2014 Numérigraphe SARL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from collections import Counter
 
-from openerp import models, fields, api
-from openerp.addons import decimal_precision as dp
+from odoo import models, fields, api
 
-from openerp.exceptions import AccessError
+from odoo.exceptions import AccessError, UserError
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-
-    potential_qty = fields.Float(
-        compute='_get_potential_qty',
-        type='float',
-        digits_compute=dp.get_precision('Product Unit of Measure'),
-        string='Potential',
-        help="Quantity of this Product that could be produced using "
-             "the materials already at hand.")
 
     # Needed for fields dependencies
     # When self.potential_qty is compute, we want to force the ORM
@@ -30,56 +20,58 @@ class ProductProduct(models.Model):
     )
 
     @api.multi
-    @api.depends('potential_qty')
-    def _immediately_usable_qty(self):
+    def _compute_available_quantities_dict(self):
         """Add the potential quantity to the quantity available to promise.
 
         This is the same implementation as for templates."""
-        super(ProductProduct, self)._immediately_usable_qty()
+        res = {}
         for product in self:
-            product.immediately_usable_qty += product.potential_qty
+            potential_qty = self._get_potential_qty(product)
+            res[product.id] = {
+                'immediately_usable_qty': product.virtual_available +
+                potential_qty,
+                'potential_qty': potential_qty
+            }
+        return res
 
     @api.multi
-    @api.depends('component_ids.potential_qty')
-    def _get_potential_qty(self):
+    @api.depends('virtual_available',
+                 'component_ids.potential_qty',
+                 'component_ids.virtual_available')
+    def _compute_available_quantities(self):
+        super()._compute_available_quantities()
+
+    def _get_potential_qty(self, product):
         """Compute the potential qty based on the available components."""
         bom_obj = self.env['mrp.bom']
-        uom_obj = self.env['product.uom']
+        bom = bom_obj._bom_find(product=product)
+        if not bom:
+            return 0.0
 
-        for product in self:
-            bom_id = bom_obj._bom_find(product_id=product.id)
-            if not bom_id:
-                product.potential_qty = 0.0
-                continue
+        # Need by product (same product can be in many BOM lines/levels)
+        try:
+            component_needs = self._get_components_needs(product, bom)
+        except AccessError:
+            # If user doesn't have access to BOM
+            # he can't see potential_qty
+            component_needs = None
+        if not component_needs:
+            # The BoM has no line we can use
+            product.potential_qty = 0.0
 
-            bom = bom_obj.browse(bom_id)
+        else:
+            # Find the lowest quantity we can make with the stock at hand
+            components_potential_qty = min(
+                [self._get_component_qty(component) // need
+                 for component, need in component_needs.items()]
+            )
 
-            # Need by product (same product can be in many BOM lines/levels)
-            try:
-                component_needs = self._get_components_needs(product, bom)
-            except AccessError:
-                # If user doesn't have access to BOM
-                # he can't see potential_qty
-                component_needs = None
-
-            if not component_needs:
-                # The BoM has no line we can use
-                product.potential_qty = 0.0
-
-            else:
-                # Find the lowest quantity we can make with the stock at hand
-                components_potential_qty = min(
-                    [self._get_component_qty(component) // need
-                     for component, need in component_needs.items()]
-                )
-
-                # Compute with bom quantity
-                bom_qty = uom_obj._compute_qty_obj(
-                    bom.product_uom,
-                    bom.product_qty,
-                    bom.product_tmpl_id.uom_id
-                )
-                product.potential_qty = bom_qty * components_potential_qty
+            # Compute with bom quantity
+            bom_qty = bom.product_uom_id._compute_quantity(
+                bom.product_qty,
+                bom.product_tmpl_id.uom_id
+            )
+            return bom_qty * components_potential_qty
 
     def _get_component_qty(self, component):
         """ Return the component qty to use based en company settings.
@@ -87,7 +79,7 @@ class ProductProduct(models.Model):
         :type component: product_product
         :rtype: float
         """
-        icp = self.env['ir.config_parameter']
+        icp = self.env['ir.config_parameter'].sudo()
         stock_available_mrp_based_on = icp.get_param(
             'stock_available_mrp_based_on', 'qty_available'
         )
@@ -101,20 +93,20 @@ class ProductProduct(models.Model):
         :type bom: mrp_bom
         :rtype: collections.Counter
         """
-        bom_obj = self.env['mrp.bom']
-        uom_obj = self.env['product.uom']
-        product_obj = self.env['product.product']
-
         needs = Counter()
-        for bom_component in bom_obj._bom_explode(bom, product, 1.0)[0]:
-            product_uom = uom_obj.browse(bom_component['product_uom'])
-            component = product_obj.browse(bom_component['product_id'])
-
-            component_qty = uom_obj._compute_qty_obj(
-                product_uom,
-                bom_component['product_qty'],
-                component.uom_id,
-            )
+        for bom_component in bom.explode(product, 1.0)[1]:
+            component = bom_component[0]['product_id']
+            try:
+                line_uom = bom_component[0].product_uom_id
+                component_qty = line_uom._compute_quantity(
+                    bom_component[1]['original_qty'],
+                    component.uom_id
+                )
+            except UserError:
+                # This occurs, when we have components
+                # which are in a different cateogry.
+                continue
+            component_qty = component_qty * bom_component[1]['qty']
             needs += Counter(
                 {component: component_qty}
             )
