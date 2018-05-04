dc7a5a4b9e165cf1bcd3e72c1e4c522631965960
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-05-04 16:29:29 +0200

    add small comment

--- stock_available_mrp/models/product_product.py
+++ stock_available_mrp/models/product_product.py
@@ -8,9 +8,15 @@ from odoo.exceptions import AccessError, UserError
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    # Needed for fields dependencies
-    # When self.potential_qty is compute, we want to force the ORM
-    # to compute all the components potential_qty too.
+# Needed for fields dependencies
+# As self.potential_qty is computed, we want to force the ORM to compute
+# the potential_qty of all the components of the actual product first.
+# Although component_ids isn't stored,
+# is never accessed and isn't actually computed, we need this field to ensure
+# the dependency chain of _compute_potential_qty is correct.
+# with the field potential_qty being marked as recursive.
+# (cf setup_triggers
+# https://github.com/odoo/odoo/blob/11.0/odoo/fields.py#L684)
     component_ids = fields.Many2many(
         comodel_name='product.product',
         compute='_compute_component_ids',
